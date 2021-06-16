462ed2bce99303ece78454030971afe632684c65
twelvesgarnet <ehdcks112@naver.com>
2021-06-16 12:32:19 +0900

    OJDBC6, MyBatis, MyBatis-Spring, spring-jdbc, log4jdbc, commons fileupload, jackson databind 설정 후 커밋

---
+++ src/main/java/com/jdc/www/TestController.java
@@ -0,0 +1,8 @@
+package com.jdc.www;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestController {
+	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+}
